94e623a8c1a55ab33a212dccc0888965b68bebf5
gracelove <gracelove@DESKTOP-1I1P91F>
2019-04-21 13:24:39 +0900

    init shuffle method

--- src/java1000q/QuizEx1.java
+++ src/java1000q/QuizEx1.java
@@ -1,36 +1,63 @@
 package java1000q;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class QuizEx1 {
-	public static void main(String[] args) {
-		String[] data = {
-                "다음 중 키워드가 아닌 것은?`2`final`True`if`public",
-                "다음 중 자바의 연산자가 아닌 것은?`5`&`|`++`!=`/`^",
-                "다음 중 메서드의 반환값이 없음을 의미하는 키워드는?`1`void`null`false",
-          }; 
+	 public static void main(String[] args) {
+         String[] data = {
+               "다음 중 키워드가 아닌 것은?`2`final`True`if`public",
+               "다음 중 자바의 연산자가 아닌 것은?`6`&`|`++`!=`/`^",
+               "다음 중 메서드의 반환값이 없음을 의미하는 키워드는?`1`void`null`false`",
+         };
 
-          for(int i=0;i<data.length;i++) {
-        	  String[] temp = data[i].split("`", 2);
-        	  String question = temp[0];
-        	  String[] temp2 = temp[1].split("`", 2);
-        	  String answer = temp2[0];
-        	  String[] selection = temp2[1].split("`");
-        	  
-        	  System.out.println(question);
-        	  for(int j = 0; j<selection.length; j++) {
-        		  System.out.printf("%d)%s\t\t" , j+1, selection[j]);
-        	  }
-        	  System.out.println();
-        	  
-        	  
-        	  
-        	     // 1. String클래스의 String[] split(String regex, int limit)를 사용해서 문제와 선택지를 나누세요.
-                 // 2. 문제를 출력하세요.
-                 // 3. 선택지를 나누기 위해 String[] split(String regex)를 사용하세요.
-                 // 4.반복문을 이용해서 선택지를 출력하세요.
+		Scanner s = new Scanner(System.in);
+		int score = 0;
+		shuffle(data); // 문제를 섞는다.
+		
+		for (int i = 0; i < data.length; i++) {
+			String[] tmp = data[i].split("`", 3);
 
-          }
-    	 
-    } // main
+			String question = tmp[0];
+			String answer = tmp[1];
+			String[] choices = tmp[2].split("`");
+
+			answer = choices[Integer.parseInt(answer) - 1];
+			shuffle(choices); // 선택지를 섞는다.
+			System.out.println("[" + (i + 1) + "] " + question);
+			for (int x = 0; x < choices.length; x++) {
+				System.out.printf(choices[x]+"\t");
+
+			}
+
+			System.out.println();
+			System.out.print("[답]");
+			String input = s.nextLine();
+
+			if (input.equals(answer)) {
+				score++;
+			}
+
+			System.out.println();
+			System.out.println();
+		}
+
+		System.out.println("정답개수/전체문항수 :" + score + "/" + data.length);
+	} // main
+
+	public static void shuffle(String[] data) {
+		if(data.length <= 0) return;
+		
+		for(int i = 0; i < data.length * 2; i++) {
+			int ranIndex = (int)(Math.random() * data.length);
+			String tmp = data[0];
+			data[0] = data[ranIndex];
+			data[ranIndex] = tmp;
+		}
+		// 코드를 완성하세요.
+
+		// 1. 배열data의 개수가 0보다 같거나 적으면 메서드 전체를 빠져나간다.
+
+		// 2. 선택지의 순서를 뒤바꾼다. 반복문과 Math.random()사용
+	} // shuffle()
 }
+
